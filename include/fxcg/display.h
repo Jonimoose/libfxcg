fb0ab23e72cf3bca64c6f8b2063b4ca9d1ba6611
gbl08ma <gbl08ma@gmail.com>
2012-12-21 14:42:44 +0000

    Comment out a struct on include/fxcg/display.h which wouldn't compile.

--- include/fxcg/display.h
+++ include/fxcg/display.h
@@ -41,14 +41,14 @@ unsigned short Bdisp_GetPoint_DD( int x, int y );
 void DirectDrawRectangle( int x1, int y1, int x2, int y2, unsigned short color );
 void HourGlass( void );
 //Shape drawing:
-struct TShapeProps{
+/*struct TShapeProps{
 	int dx;
 	int dy;
 	int wx;
 	int wy;
 	int color;
 	TBdispFillArea saved;
-};
+};*/
 void Bdisp_ShapeBase3XVRAM( void*shape );
 void Bdisp_ShapeBase( unsigned char*work, void*shape, int color, int line_width, int zero1, int zero2 );
 void Bdisp_ShapeToVRAM16C( void*, int color );
