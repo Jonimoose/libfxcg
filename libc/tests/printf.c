ea64f822e1695fde9d0c3c74e074e8c491f25db7
Peter Marheine <peter@taricorp.net>
2014-07-24 17:59:54 -0400

    Reconfiguration for GCC 4.9 target sh4aeb-nofpu-elf

--- libc/tests/printf.c
+++ libc/tests/printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    fprintf(stdout, "");
     fprintf(stdout, "Hello!\n");
     fprintf(stdout, "%u..%u..%u.. YAY!\n", 1, 2, 3);
     fprintf(stdout, "EOF is %i\n", -1);
