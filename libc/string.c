d43abb1fb4c92ed3c1189a7012f9304ce1913daf
gbl08ma <gbl08ma@gmail.com>
2014-08-26 23:47:30 +0100

    On strncmp and strncasecmp, handle the max. size being zero and correctly return 0.

--- libc/string.c
+++ libc/string.c
@@ -133,6 +133,7 @@ char *strncat(char *dest, const char *src, size_t num) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
+	if(n <= 0) return 0;
 	while (*s1 != 0 && *s2 != 0) {
 		n--;
 		if (*s1 != *s2 || n == 0)
@@ -145,6 +146,7 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 int strncasecmp(const char *s1, const char *s2, size_t n) {
+	if(n <= 0) return 0;
 	while (*s1 != 0 && *s2 != 0) {
 		n--;
 		if (tolower(*s1) != tolower(*s2) || n == 0)
