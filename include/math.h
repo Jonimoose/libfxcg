d1099edf98297218b4ecf74d2976b6924fba5a3e
dr-carlos <77367421+dr-carlos@users.noreply.github.com>
2022-03-20 15:40:39 +1030

    Remove unimplemented abs prototypes

--- include/math.h
+++ include/math.h
@@ -25,9 +25,6 @@ extern "C" {
 #define M_SQRT2 1.41421356237309504880    /* sqrt(2) */
 #define M_SQRT1_2 0.70710678118654752440  /* 1/sqrt(2) */
 
-double fabs(double x);
-long labs(long n);
-
 #ifdef __cplusplus
 }
 #endif
