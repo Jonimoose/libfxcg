736179cc17be9f93b7cddd98e5f56995fd010a79
dr-carlos <77367421+dr-carlos@users.noreply.github.com>
2022-10-26 09:35:42 +1030

    Delete duplicate syscall headers

--- include/RTC_syscalls.h
+++
@@ -1,5 +0,0 @@
-int RTC_Elapsed_ms( int start_value, int duration_in_ms );
-int RTC_GetTicks(void);
-int RTC_Reset( int );
-void RTC_GetTime( unsigned int*hour, unsigned int*minute, unsigned int*second, unsigned int*millisecond );
-void RTC_SetDateTime( unsigned char*timestr );
--- include/system.h
+++
@@ -1,3 +0,0 @@
-int GetMainBatteryVoltage( int one );
-void*GetStackPtr( void );
-void OS_InnerWait_ms( int );
