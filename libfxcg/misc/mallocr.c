90d2d0ab1968da921dc2c8aebc1cf7d1f3bbdf8a
dr-carlos <77367421+dr-carlos@users.noreply.github.com>
2023-06-12 08:52:12 +0930

    Remove reentrant dynamic memory functions

--- libfxcg/misc/mallocr.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-void *
-_malloc_r (struct _reent *ptr, int size)
-{
-  return malloc (size);
-}
--- libfxcg/misc/reallocr.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-void *
-_realloc_r (struct _reent *ptr, void *old, int newlen)
-{
-  return realloc (old, newlen);
-}
